adc026dccc3f64fdbe792200a9de6786146b674e
monsa849 <54692236+monsa849@users.noreply.github.com>
2020-07-20 16:02:12 -0400

    Add files via upload

---
+++ lyrics/love.js
@@ -0,0 +1,15 @@
+function changeText(id) {
+  id.innerHTML = "Lost";
+}
+
+function continuePhrase(id) {
+	id.innerHTML = "Hold These Hands<br>Take Me As I Am"
+}
+
+function continueThought(id) {
+	id.innerHTML = "I Come to You With Words"
+}
+
+function changeMe(id) {
+	id.innerHTML = "Toy"
+}
---
+++ lyrics/people.js
@@ -0,0 +1,16 @@
+
+function on() {
+  document.getElementById("drama").style.display = "inline";
+}
+
+function off() {
+  document.getElementById("drama").style.display = "none";
+}
+
+function on() {
+	document.getElementById("yes").style.display = "inline";
+}
+
+function off() {
+	document.getElementById("yes").style.display = "none";
+}
